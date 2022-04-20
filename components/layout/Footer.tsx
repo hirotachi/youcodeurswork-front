79ff1be2a64767900b15658e858ec34014c5d8de
Said Oudouane <saidoudouane@gmail.com>
2022-04-20 23:10:48 +0000

    add footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React from "react";
+import styles from "@modules/Footer.module.scss";
+import Link from "next/link";
+
+const socials = [
+  {
+    name: "facebook",
+    url: "https://www.facebook.com/YouCodeSchool/",
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.com/youcode18",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/_youcode_/",
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/company/youcode/",
+  },
+];
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className={styles.footer}>
+      <Link href={"/"}>
+        <a className={styles.logo}>
+          <span className={styles.you}>You</span>
+          <span className={styles.code}>Code</span>
+          <span className={styles.copy}>&copy;{new Date().getFullYear()}</span>
+        </a>
+      </Link>
+      <div className={styles.socials}>
+        <span className={styles.follow}>Follow us</span>
+        {socials.map((social) => (
+          <a
+            key={social.name}
+            href={social.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {social.name}
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
